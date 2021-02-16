d61998ae10ea7d0cd2cfd982a2f054e7b345102e
ZoombieZoom <75949227+ZoombieZoom@users.noreply.github.com>
2021-02-16 13:23:14 +0900

    userモデルバリデート追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,19 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/
-  validates :password, presence: true,
-            format: { with: VALID_PASSWORD_REGEX,
-             message: "は半角6~12文字英大文字・小文字・数字それぞれ１文字以上含む必要があります"}
+  validates :password, presence: true, 
+            format: { with: VALID_PASSWORD_REGEX, message: "は半角6~12文字英大文字・小文字・数字それぞれ１文字以上含む必要があります"}
   
-  with_options presence: true do
-    with_options format: {with: /\A[ぁ-んァ-ン一-龥]/}
-      validates :kanji_first_name
-      validates :kanji_last_name
-    end
+  with_options format: {with: /\A[ぁ-んァ-ン一-龥]/} do
+    validates :kanji_first_name
+    validates :kanji_last_name
+  end
 
-    with_options format: {with: /\A[ァ-ヶー－]+\z/}
-      validates :kana_first_name
-      validates :kana_last_name
-    end
+  with_options format: {with: /\A[ァ-ヶー－]+\z/} do
+    validates :kana_first_name
+    validates :kana_last_name
   end
 end
+
